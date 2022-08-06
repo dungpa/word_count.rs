7e2171098d23900a9c477b45e097774fdd99980c
Anh-Dung Phan <phananhdung309@yahoo.com>
2022-08-06 20:34:13 +0100

    Sort results

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ fn main() {
         let words = word_counter::split_into_words(lines);
         let word_counts = word_counter::count_words(words);
 
-        for (key, value) in word_counts.into_iter() {
-            println!("{}: {}", key, value.len());
+        for (key, value) in word_counts {
+            println!("{}: {}", key, value);
         }
     }
 }
--- src/word_counter.rs
+++ src/word_counter.rs
@@ -1,6 +1,5 @@
 use std::fs::File;
 use std::io::{self};
-use std::collections::HashMap;
 use regex::Regex;
 use itertools::Itertools;
 
@@ -18,11 +17,14 @@ pub fn split_into_words(lines: io::Lines<io::BufReader<File>>) -> Vec<String> {
     words   
 }
 
-pub fn count_words(words: Vec<String>) -> HashMap<String, Vec<String>> {
+pub fn count_words(words: Vec<String>) -> impl Iterator<Item=(usize, String)> {
     // TODO: Remove unnecessary clones.
-    let hash_map = 
+    let counts = 
         words.into_iter()
              .filter_map(|s| { if s.trim().is_empty() { None } else { Some((s.clone(), s.clone())) } })
-             .into_group_map();
-    hash_map
+             .into_group_map()
+             .into_iter()
+             .map (|(key, value)| (value.len(), key))
+             .sorted();
+    counts
 }
