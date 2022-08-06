a355bef88074220e31f01c0fcd86962c9021b826
Anh-Dung Phan <phananhdung309@yahoo.com>
2022-08-06 16:26:51 +0100

    Split lines into words

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use regex::Regex;
 
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
@@ -11,12 +12,16 @@ where P: AsRef<Path>, {
 }
 
 fn main() {
-    // File hosts must exist in current path before this produces output
+    let word_separator = Regex::new(r"([ ,.]+)").expect("Invalid regex");
+
     if let Ok(lines) = read_lines("sample_50_words.txt") {
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
             if let Ok(ip) = line {
-                println!("{}", ip);
+                let splits: Vec<_> = word_separator.split(&ip).into_iter().collect();
+                for split in splits {
+                    println!("\"{}\"", split);
+                }
             }
         }
     }
