18de5505281fdf5ed37edb334870020e39fc21a1
Anh-Dung Phan <phananhdung309@yahoo.com>
2022-08-06 20:45:50 +0100

    Measure calculation time

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::time::{Instant};
 mod file_reader;
 mod word_counter;
 
@@ -8,10 +9,14 @@ fn main() {
         println!("Usage: word_count.exe input_file.txt");
     }
     else {
+        let start = Instant::now();
         let input_file_name = &args[1];
         if let Ok(lines) = file_reader::read_lines(input_file_name) {
             let words = word_counter::split_into_words(lines);
             let word_counts = word_counter::count_words(words);
+
+            let duration = start.elapsed();
+            println!("Time elapsed in word counting is: {:?}", duration);
 
             for (key, value) in word_counts {
                 println!("{}: {}", key, value);
