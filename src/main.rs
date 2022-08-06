40c4b4f93891d2657c8aefd3b1a482544c245213
Anh-Dung Phan <phananhdung309@yahoo.com>
2022-08-06 16:58:57 +0100

    Collect all strings into a vector

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,17 @@ fn main() {
     let word_separator = Regex::new(r"([ ,.]+)").expect("Invalid regex");
 
     if let Ok(lines) = file_reader::read_lines("sample_50_words.txt") {
+        let mut words: Vec<String> = vec![];
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
             if let Ok(ip) = line {
-                let splits: Vec<_> = word_separator.split(&ip).into_iter().collect();
-                for split in splits {
-                    println!("\"{}\"", split);
-                }
+                let new_words: Vec<String> = word_separator.split(&ip).map(str::to_string).collect();
+                words.extend(new_words);
             }
+        }
+
+        for word in words {
+            println!("\"{}\"", word);
         }
     }
     println!("Done");
