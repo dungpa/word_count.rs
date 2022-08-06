ab674a176a6adeca3366fe80ac3ff65c15a8c2b7
Anh-Dung Phan <phananhdung309@yahoo.com>
2022-08-06 16:32:43 +0100

    Use a separate module for reading files

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,10 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
 use regex::Regex;
-
-// The output is wrapped in a Result to allow matching on errors
-// Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
+mod file_reader;
 
 fn main() {
     let word_separator = Regex::new(r"([ ,.]+)").expect("Invalid regex");
 
-    if let Ok(lines) = read_lines("sample_50_words.txt") {
+    if let Ok(lines) = file_reader::read_lines("sample_50_words.txt") {
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
             if let Ok(ip) = line {
