b8069d769a6576dbdc91d5b7a10ae9ab2c5d09bf
Anh-Dung Phan <phananhdung309@yahoo.com>
2022-08-08 14:55:45 +0100

    Add basic tests

--- src/word_counter.rs
+++ src/word_counter.rs
@@ -11,3 +11,22 @@ pub fn count_words(words: Vec<String>) -> impl Iterator<Item=(usize, String)> {
              .sorted();
     counts
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn count_words_works_on_empty_input() {
+        let input = vec![];
+        let expected = vec![];
+        let actual = crate::word_counter::count_words(input);
+        itertools::assert_equal(actual, expected);
+    }
+
+    #[test]
+    fn count_words_works_on_words_with_single_occurences() {
+        let input = vec!["Hello".to_string(), "world".to_string()];
+        let expected = vec![(1, "Hello".to_string()), (1, "world".to_string())];
+        let actual = crate::word_counter::count_words(input);
+        itertools::assert_equal(actual, expected);
+    }
+}
