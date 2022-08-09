8c234aff5bffbbbb9db4d03f6dabd67b324b5b4a
Anh-Dung Phan <phananhdung309@yahoo.com>
2022-08-09 20:56:20 +0100

    Fix TODOs

--- src/word_counter.rs
+++ src/word_counter.rs
@@ -1,10 +1,9 @@
 use itertools::Itertools;
 
 pub fn count_words(words: Vec<String>) -> impl Iterator<Item=(usize, String)> {
-    // TODO: Remove unnecessary clones.
     let counts = 
         words.into_iter()
-             .filter_map(|s| { if s.trim().is_empty() { None } else { Some((s.to_lowercase().clone(), s.clone())) } })
+             .filter_map(|s| { if s.trim().is_empty() { None } else { Some((s.to_lowercase(), s)) } })
              .into_group_map()
              .into_iter()
              .map (|(key, value)| (value.len(), key))
